31f6304d08787d02917c5f55a6e4f01f1e8f40c4
0xanonymous007 <ayush.sharma@eiysys.in>
2023-11-20 05:31:52 +0530

    .

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/readLand/:landID", async (req, res) => {
     const privateKey = crypto.createPrivateKey(privateKeyPem);
     const signer = signers.newPrivateKeySigner(privateKey);
 
-    const client = new grpc.Client('peer1.example.reg.com:7002', grpc.credentials.createInsecure());
+    const client = new grpc.Client('grpcs://172.31.89.52:7002', grpc.credentials.createInsecure());
     const gateway = connect({ identity, signer, client });
     
         const network = gateway.getNetwork('autochannel');
